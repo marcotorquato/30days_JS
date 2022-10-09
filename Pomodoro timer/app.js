5bd35e13403031dfdb7090641941093e94bcb4ea
marcotorquato <marcotorquato97@gmail.com>
2022-10-09 21:47:52 +0200

    add tag AdSense

--- Pomodoro timer/app.js
+++ Pomodoro timer/app.js
@@ -143,3 +143,9 @@ $(document).ready(function() {
     initialSeconds = time;
   }
 });
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CZP89Z491V');
